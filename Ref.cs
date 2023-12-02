02857034375938fc692483c22b95bb3292947f37
olimov Amir <olimov.amir@mail.ru>
2023-12-02 18:47:08 +0300

    Добавьте файлы проекта.

---
+++ Ref.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ref
+{
+    struct MyStract
+    {
+        public int a;
+        public double b;
+        public float c;
+    }
+    class Ref
+    {
+        static void Foo(ref MyStract myStract)
+        {
+            myStract.a = -5;
+        }
+
+        static void Bar(int[] array) 
+        {
+            array[0] = -5;
+        }
+        static void Main(string[] args)
+        {
+          int a = 2;
+            MyStract myStract = new MyStract();
+            Foo(ref myStract);
+
+            Console.WriteLine(a);
+
+            //------------------------------------
+
+            int[] myArray = { 1, 4, 6 };
+
+            Bar(myArray);
+        }
+    }
+}
